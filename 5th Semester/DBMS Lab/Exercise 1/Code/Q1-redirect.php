ec899ff3806ad201ad727fe84adcaa7817b395c2
TOM SAJU <76404537+TomSaju2001@users.noreply.github.com>
2021-08-09 13:59:59 +0530

    Q1 Added

--- 5th Semester/DBMS Lab/Exercise 1/Code/Q1-redirect.php
+++ 5th Semester/DBMS Lab/Exercise 1/Code/Q1-redirect.php
@@ -1,59 +1,72 @@
-<!-- Tom Saju | B191290CS | DBMS LAB EXERCISE 1 -->
+<?php 
+// Tom Saju | B191290CS | DBMS LAB EXERCISE 1
+$conn = mysqli_connect('localhost', 'root', '', 'exercise1');
+
+if (!$conn) {
+	die("Connection ERROR". mysqli_connect_error());
+}
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+    $rollNo = $_POST['rollNo'];
+    $DOB = $_POST['DOB'];
+    $address = $_POST['address'];
+    $mobileNo =$_POST['mobileNo'];
+    $email = $_POST['email'];
+    $s1 = $_POST['s1'];
+    $s2 = $_POST['s2'];
+    $s3 = $_POST['s3'];
+    $s4 = $_POST['s4'];
+    $s5 = $_POST['s5'];
+    $s6 = $_POST['s6'];
+    $s7 = $_POST['s7'];
+    $s8 = $_POST['s8'];
+    $cgpa = $_POST['cgpa'];
+    $hobbies =  $_POST['hobbies'];
+    $h_or_d =  $_POST['h_or_d'];
+    $ref =  $_POST['ref'];
+}
+
+
+$sql = "INSERT INTO `exercise1` (`name`, `rollNo`, `DOB`, `address`, `mobileNo`, `email`, `s1`, `s2`, `s3`, `s4`, `s5`, `s6`, `s7`, `s8`, `cgpa`, `hobbies`, `h_or_d`, `ref`) VALUES ('$name', '$rollNo', '$DOB', '$address', '$mobileNo', '$email', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7', '$s8', '$cgpa', '$hobbies', '$h_or_d', '$ref');";
+
+ if(!(mysqli_query($conn,$sql))){
+        echo mysqli_error($conn);
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Student Biodata</title>
-    <link href="https://fonts.googleapis.com/css?family=Merriweather|Montserrat|Sacramento" rel="stylesheet" >
-
-    <style type="text/css">
-        body{
-            background-color: #055052;
-            font-size: 1.5em;
-        }
-        * {
-            box-sizing: border-box;
-        }
-
-        h1.biodata{
-            text-align: center;
-            margin-top: 20px;
-            margin-bottom: 20px;
-            font-family: 'Sacramento', cursive;
-            font-size: 2.5em;
-            color: #53B8BB;
-            font-weight: bolder;
-        }
-
-        h4{
-            margin-bottom: 10px;
-        }
-
-        .main-div{
+       <meta charset="utf-8">
+       <title>Student Biodata</title>
+       <style type="text/css">
+          body {
+              background-color: #055052;
+              font-size: 1.5em;
+          }
+          .main-div{
             line-height: 1.3;
             color: #F3F2C9;
             margin-bottom: 30px;
-        }
+          }
 
-        table{
+          h3.thankYou{
+            text-align: center;
+            margin-top: 20px;
+            margin-bottom: 40px;
+            font-family: 'Sacramento', cursive;
+            font-size: 1.5em;
+            color: #53B8BB;
+            font-weight: bolder;
+          }
+
+          table{
             margin-left: auto;
             margin-right: auto;
-        }
-
-        input {
-            width: 80%;
-            margin-left: 10px;
-        }
-
-        textarea{
-            margin-left: 10px;
-        }
-
-        select {
-            margin-left: 10px;
-        }
-
-        .btn{
+          }
+          .btn{
             text-align: center;
             background: #11cdd4;
             background-image: -webkit-linear-gradient(top, #11cdd4, #11999e);
@@ -80,46 +93,43 @@
             background-image: linear-gradient(to bottom, #30e3cb, #2bc4ad);
             text-decoration: none;
         }
-
-    </style>
+       </style>
 </head>
 <body>
-    <header>
-        <h1 class="biodata">BIODATA</h1>
-    </header>
-
-    <main>
-        <div class="main-div">
-        <form action="Q1-redirect.php" method="post">
-            <table class="form-table">
-                <tbody>
-                    <tr>
+       <header>
+        <h3 class="thankYou">Thank You, Your Submission has been recieved.!</h3>
+       </header>
+       <div class="main-div">
+             <table>
+              <tbody>
+                     <tr>
                         <td><label>NAME</label></td>
                         <td>:</td>
-                        <td><input type="text" name="name" placeholder="Student Name"></td>
+                        <td><?php echo $name; ?></td>
                     </tr>
+                    <tr>
                         <td><label>Roll Number</label></td>
                         <td>:</td>
-                        <td><input type="text" name="rollNo" placeholder="Student Roll No:"></td>
+                        <td><?php echo $rollNo; ?></td>
                     <tr>
                         <td><label>Date Of Birth</label></td>
                         <td>:</td>
-                        <td><input type="date" name="DOB" placeholder="DOB"></td>
+                        <td><?php $DOB; ?></td>
                     </tr>
                     <tr>
                         <td><label>Address</label></td>
                         <td>:</td>
-                        <td><textarea name="address" placeholder="Address" rows="4" cols="31"></textarea></td>
+                        <td><?php echo $address; ?></textarea></td>
                     </tr>
                     <tr>
                         <td><label>Mobile Number</label></td>
                         <td>:</td>
-                        <td><input type="text" name="mobileNo" placeholder="Mobile Number"></td>
+                        <td><?php echo $mobileNo; ?></td>
                     </tr>
                     <tr>
                         <td><label>E-mail ID</label></td>
                         <td>:</td>
-                        <td><input type="text" name="email" placeholder="Email Id"></td>
+                        <td><?php echo $email; ?></td>
                     </tr>
 
                     <tr>
@@ -129,42 +139,42 @@
                     <tr>
                         <td><label>Semester 1</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s1" value="0"></td>
+                        <td><?php echo $s1; ?></td>
                     </tr>
                     <tr>
                         <td><label>Semester 2</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s2" value="0"></td>
+                        <td><?php echo $s2; ?></td>
                     </tr>
                     <tr>
                         <td><label>Semester 3</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s3" value="0" placeholder="sgpa"></td>
+                        <td><?php echo $s3; ?></td>
                     </tr>
                     <tr>
                         <td><label>Semester 4</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s4" value="0" placeholder="sgpa"></td>
+                        <td><?php echo $s4; ?></td>
                     </tr>
                     <tr>
                         <td><label>Semester 5</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s5" value="0" placeholder="sgpa"></td>
+                        <td><?php echo $s5; ?></td>
                     </tr>
                     <tr>
                         <td><label>Semester 6</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s6" value="0" placeholder="sgpa"></td>
+                        <td><?php echo $s6; ?></td>
                     </tr>
                     <tr>
                         <td><label>Semester 7</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s7" value="0" placeholder="sgpa"></td>
+                        <td><?php echo $s7; ?></td>
                     </tr>
                     <tr>
                         <td><label>Semester 8</label></td>
                         <td>:</td>
-                        <td><input type="text" name="s8" value="0"  placeholder="sgpa"></td>
+                        <td><?php echo $s8; ?></td>
                     </tr>
                     <tr>
                         <td><br></td>
@@ -174,43 +184,35 @@
                     <tr>
                         <td><label>CGPA</label></td>
                         <td>:</td>
-                        <td> <input type="text" name="cgpa" value="0" placeholder="cgpa"></td>
+                        <td><?php echo $cgpa; ?></td>
                     </tr>
                     <tr>
                         <td><br></td>
                         <td><br></td>
-                        <td><br></td>
+                        <td><?php  ?><br></td>
                     </tr>
                     <tr>
                         <td><label>Hobbies</label></td>
                         <td>:</td>
-                        <td><textarea type="text" name="hobbies" placeholder="Enter Your Hobbies Row-Wise" rows="4" cols="31" style="margin-right: 59px;"></textarea></td>
+                        <td><?php echo $hobbies; ?></td>
                     </tr>
                     <tr>
                         <td><label>Hosteller or Day scholar</label></td>
                         <td>:</td>
-                        <td>
-                            <select name="h_or_d" style="width: 240px;">
-                                <option value="Hosteller">Hosteller</option>
-                                <option value="Day scholar">Day scholar</option>
-                            </select>
-                        </td>
+                        <td><?php echo $h_or_d; ?></td>
                     </tr>
                     <tr>
                         <td><label>References</label></td>
                         <td>:</td>
-                        <td><textarea type="text" name="ref" placeholder="References" rows="4" cols="31"></textarea></td>
+                        <td><?php echo $ref; ?></td>
                     </tr>
-                </tbody>
-            </table>
-            <br>
-            <div style="text-align: center;">
-                <button type="submit" name="submit" class="btn">SUBMIT BIODATA
-                </button>
-            </div>
-                
-        </form>
+              </tbody>
+       </table> 
+       <div style="text-align: center; margin-top: 50px;">
+             <a href="Q1.php" class="btn">Enter Again</a> 
        </div>
-    </main>
+       
+       </div>
+       
 </body>
 </html>
