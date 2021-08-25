da6b6ffb12927e81f81223393f7faf2f8dd387b0
TOM SAJU <76404537+TomSaju2001@users.noreply.github.com>
2021-08-25 22:59:06 +0530

    added 1&2

---
+++ 3rd Semester/Logic Design Lab/Assignment 1/Solutions/Tom_B191290CS_Q12.v
@@ -0,0 +1,43 @@
+module NotGate(a,b) ;  
+
+output b ;
+input a ;
+
+nand(b,a,a) ;
+
+endmodule 
+
+module AndGate(a,b,c) ; 
+
+output c ;
+input a,b ;
+wire x ;
+
+nand(x,a,b);
+nand(c,x,x) ;
+
+endmodule 
+
+module OrGate(a,b,c) ;  
+output c ;
+input a,b ;
+wire x,y ;
+
+nand(x,a,a) ;
+nand(y,b,b) ;
+nand(c,x,y) ;
+
+endmodule 
+
+module Mux2x1(a,b,s,c) ;
+
+output c ;
+input a,b,s ;
+wire x,y,z ;
+
+NotGate NG_1(s,z );
+AndGate AG_1(a,z,x) ;
+AndGate AG_2(b,s,y) ;
+OrGate OG_1(x,y,c) ;
+
+endmodule 
